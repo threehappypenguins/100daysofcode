035debb6fa54229811bf2ae73b028c93777ddfd9
Sarah Poulin <threehappypenguins@gmail.com>
2024-04-29 14:55:13 -0300

    Completed Day 3 NPM Basics by using npm init and installing axios, and utilizing it in a script.

---
+++ nodejs/day003-npm-basics-axios.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log('Response data:', response.data);
+  })
+  .catch(error => {
+    console.error('Error', error);
+  });
