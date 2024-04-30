e7c13f89b71fea128fb08b47bb4ac925b13fe0c9
Sarah Poulin <threehappypenguins@gmail.com>
2024-04-30 16:22:50 -0300

    Updated Day 9 folder for organizational purposes

--- nodejs/day009-static-files-w-express.js
+++ nodejs/day009-static-files-w-express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static('day009-public'));
 
 // Start the express server
 const port = 3000;
