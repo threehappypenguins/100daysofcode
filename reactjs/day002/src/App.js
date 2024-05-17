7ec837e4b269ec18add33672f672f3f03ce9e592
Sarah Poulin <threehappypenguins@gmail.com>
2024-05-17 15:50:18 -0300

    Completed Day 2 JSX Basics

---
+++ reactjs/day002/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, JSX!</h1>
+      <p>This is a paragraph rendered with JSX.</p>
+    </div>
+  );
+}
+
+export default App;
