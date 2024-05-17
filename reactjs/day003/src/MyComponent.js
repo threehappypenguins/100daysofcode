069a8c70ec980ccaf18c3aed9135ff8151e39116
Sarah Poulin <threehappypenguins@gmail.com>
2024-05-17 15:51:34 -0300

    Completed Day 3 Functional Components

---
+++ reactjs/day003/src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>Hello, Functional Component!</h1>
+        </div>
+    );
+};
+
+export default MyComponent;
